3290f814f4068b0d4eadca413e82dea627054fd8
Chris Barker <Chris.Barker@noaa.gov>
2018-03-07 17:16:51 -0800

    added code to do all elvels of lagekes and all, and genreate geojson

--- libgoods/setup.py
+++ libgoods/setup.py
@@ -10,22 +10,21 @@ from setuptools import setup
 from libgoods import __version__
 
 setup(
-    name = "libgoods",
-    version = __version__,
-    description = "utilities for pre-processing input data for GNOME",
+    name="libgoods",
+    version=__version__,
+    description="utilities for pre-processing input data for GNOME",
     long_description=open("README.md").read(),
-    packages = ["libgoods",],
-    scripts = [
-               "scripts/hycom2bna.py",
-               "scripts/nc_time_shift.py",
-               "scripts/shape2bna",
-               "scripts/process_boem.py"
-              ],
-    author = "Amy MacFadyen, Christopher Barker",
-    author_email = "amy.macfadyen@noaa.gov, chris.barker@noaa.gov",
+    packages=["libgoods"],
+    scripts=["scripts/hycom2bna.py",
+             "scripts/nc_time_shift.py",
+             "scripts/shape2bna",
+             "scripts/process_boem.py"
+             ],
+    author="Amy MacFadyen, Christopher Barker",
+    author_email="amy.macfadyen@noaa.gov, chris.barker@noaa.gov",
     url="http://www.gnome.orr.noaa.gov",
-    license = "LICENSE.txt",
-    keywords = "GNOME GOODS ORR",
+    license="LICENSE.txt",
+    keywords="GNOME GOODS ORR",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Topic :: Utilities",
@@ -37,8 +36,5 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
-    ],
-
-    )
-
-
+                  ],
+     )
