cd045e32d97bbacc6edaaea0d4234d855ebb888d
Chris Barker <Chris.Barker@noaa.gov>
2018-11-15 17:29:09 -0800

    mostly formatting changes, plus on unility in nctools

--- libgoods/libgoods/__init__.py
+++ libgoods/libgoods/__init__.py
@@ -4,12 +4,12 @@
 __init__.py for libgoods package
 """
 
-__version__="0.1.1"
+__version__ = "0.1.2"
 
 # find data_files
 import os
 
-data_files_dir = os.path.join(os.path.split(__file__)[0],"data_files")
+data_files_dir = os.path.join(os.path.split(__file__)[0], "data_files")
 if not os.path.exists(data_files_dir):
     os.mkdir(data_files_dir)
 
